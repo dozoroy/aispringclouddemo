96c243497004acd9db289ebe2b0203bf438ed3a5
dozoroy <31701070@stu.zucc.edu.cn>
2019-12-26 14:16:36 +0800

    zipkin

--- client/src/main/java/com/dzr/ClientApplication.java
+++ client/src/main/java/com/dzr/ClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 @SpringBootApplication
 @EnableFeignClients
 @ServletComponentScan
